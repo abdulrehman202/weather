cfdecaa6ff12a73adb6e721e4487877db47ac2db
abdulrehman202 <abdulrehman202@users.noreply.github.com>
2022-12-19 15:00:46 +0500

    error card added

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -50,18 +50,18 @@ function SearchBar(){
                 <button className='submitButton' onClick={(e) => {setPanelVisibility(false)}}>
                     Close
                 </button></div>
-                <h1 >{cityName}</h1>
-                <div className='outer__weather'>
+                {weatherData.cod=='200'?(<><h1>{cityName}</h1><div className='outer__weather'>
                     <div className='inner__weather'>
-                    <span className='weather__Card'><WeatherCard heading='Co-ordinates' img = 'https://cdn-icons-png.flaticon.com/512/3293/3293841.png' data= {'Lat: '+  weatherData.coord.lat+'     Lon: '+weatherData.coord.lon} /></span>
-                    <span className='weather__Card'><WeatherCard heading='Weather' img = {'http://openweathermap.org/img/wn/'+weatherData.weather[0].icon+'@2x.png'} data= {weatherData.weather[0].main} /></span>
+                        <span className='weather__Card'><WeatherCard heading='Co-ordinates' img='https://cdn-icons-png.flaticon.com/512/3293/3293841.png' data={'Lat: ' + weatherData.coord.lat + '     Lon: ' + weatherData.coord.lon} /></span>
+                        <span className='weather__Card'><WeatherCard heading='Weather' img={'http://openweathermap.org/img/wn/' + weatherData.weather[0].icon + '@2x.png'} data={weatherData.weather[0].main} /></span>
                     </div>
 
                     <div className='inner__weather'>
-                    <span className='weather__Card'><WeatherCard heading='Temperature' img = 'https://cdn-icons-png.flaticon.com/512/3815/3815449.png' data= {'Min: '+  Math.round(weatherData.main.temp_min-273.15)+'℃' +' ~ Max: '+Math.round(weatherData.main.temp_max-273.15)+'℃' }  /></span>
-                    <span className='weather__Card'><WeatherCard heading='Humidity' img = 'https://cdn-icons-png.flaticon.com/512/481/481453.png' data= { weatherData.main.humidity}  /></span>
+                        <span className='weather__Card'><WeatherCard heading='Temperature' img='https://cdn-icons-png.flaticon.com/512/3815/3815449.png' data={'Min: ' + Math.round(weatherData.main.temp_min - 273.15) + '℃' + ' ~ Max: ' + Math.round(weatherData.main.temp_max - 273.15) + '℃'} /></span>
+                        <span className='weather__Card'><WeatherCard heading='Humidity' img='https://cdn-icons-png.flaticon.com/512/481/481453.png' data={weatherData.main.humidity} /></span>
                     </div>
-                </div>
+                </div></>):<span><WeatherCard heading='Error!' img='https://cdn-icons-png.flaticon.com/512/5974/5974771.png' data='  ' /></span>
+                        }
                 
             </div>:<div></div>}
 
